c9e34d9f05e49cd69e1ef54570418453066f5eb7
Jack H Laverick from Strath <jacklaverick@ymail.com>
2022-03-24 14:45:02 +0000

    docs: updated renders

--- R scripts/bathymetry/rayshader.01 HI-Q.R
+++ R scripts/bathymetry/rayshader.01 HI-Q.R
@@ -1,5 +1,5 @@
 
-# Pretty 3D rendering, max resolution DEM
+# Open the GEBCO netcdf file and limit to a workable size, reshape for tidyverse, and extract contours
 
 #### Set up ####
 
@@ -8,7 +8,7 @@ rm(list=ls())                                                                   
 packages <- c("tidyverse", "data.table", "ncdf4", "stars", "rayshader", "tictoc")   # List packages
 lapply(packages, library, character.only = TRUE)                                    # Load packages
 
-nc_raw <- nc_open("./Data/GEBCO_2019.nc")                                           # Access GEBCO bathymetry
+nc_raw <- nc_open("../Shared data/GEBCO_2020.nc")                                   # Access GEBCO bathymetry
 nc_lat <- ncvar_get(nc_raw, "lat")                                                  # Extract the latitudes
 nc_lon <- ncvar_get(nc_raw, "lon")                                                  # Extract the longitudes
 nc_close(nc_raw)                                                                    # You must close an open netcdf file when finished to avoid data loss
@@ -16,39 +16,47 @@ rm(nc_raw)                                                                      
 
 #### Extract Area ####
 
-S <- nrow(nc_lat)*(7.8/9) ; W <- length(nc_lon)*1/2 ; E <- length(nc_lon)*4.3/6     # For Mercatore
+S <- nrow(nc_lat)*(90-36)/180 ; N <- nrow(nc_lat)*(90-22)/180
+W <- length(nc_lon)*(180-58)/360 ; E <- length(nc_lon)*(180-41)/360     # For Mercatore
 
-Bathymetry <- read_ncdf("./Data/GEBCO_2019.nc", ncsub = cbind(
-  start = c(W, S), count =c((E-W+1), (43200 - S +1)))) #%>%
-#  st_transform(crs = 3035)  
+Bathymetry <- read_ncdf("../Shared data/GEBCO_2020.nc", ncsub = cbind(
+  start = c(W, S), count =c((E-W+1), (N-S+1)))) 
 
-# plot(Bathymetry)
+plot(Bathymetry)
 
 matrix <- Bathymetry$elevation %>% as.numeric() %>% 
   matrix(nrow = nrow(Bathymetry$elevation), ncol= ncol(Bathymetry$elevation))
 
-mat <- matrix[seq(nrow(matrix), 1, by = -3),]                               # Use for full resolution, divide zscales by 10                
+# 8192 x 8192 maximum textured syrface allowed by RGL
+mat <- matrix[seq(nrow(matrix), 1, by = -1),]                               # Use for full resolution, divide zscales by 10                
+#mat <- matrix                               # Use for full resolution, divide zscales by 10                
 
 #### Plot area ####
 
-shadow = ray_shade(mat, zscale = 0.1, lambert = FALSE, multicore = TRUE)
-ambient = ambient_shade(mat, zscale = 5, multicore = TRUE)
+montshadow = ray_shade(mat, zscale = 0.1, lambert = FALSE, multicore = TRUE)
+montamb = ambient_shade(mat, zscale = 5, multicore = TRUE)
 
 mat %>%
   sphere_shade(zscale = 1, texture = "imhof2") %>%
-  add_shadow(shadow, 0.5) %>%
-  add_shadow(ambient) %>%
-  plot_3d(mat, zscale = 20, fov = 60, theta = 60, phi = 30, 
-          windowsize = c(1170, 585), zoom = 0.3,
+  add_shadow(montshadow, 0.5) %>%
+  add_shadow(montamb) %>%
+
+  plot_3d(mat, zscale = 10, fov = 0, theta = 210, phi = 30, 
+          windowsize = c(1280, 640), zoom = 0.33,
           water = TRUE, waterdepth = 0, wateralpha = 0.75, watercolor = "lightblue",
-          waterlinecolor = "white", waterlinealpha = 0.5) 
-#z = 450, y = 800, x = -600
+          waterlinecolor = "white", waterlinealpha = 0.3,
+          asp = 1/cospi(-35/180)) 
+#Sys.sleep(30)                                                                         # Pause for RGL to open
+#render_snapshot("./Figures/bathymetry/Rayshade.png")                                  # Save the current view in the RGL window
+
+#### Rayrender ####
+
 tic()
-render_highquality(print_scene_info = T, parallel = TRUE, lightintensity = 0, samples = 1500, filename = "rayshade-hi.png", 
-                   scene_elements = bind_rows(rayrender::sphere(z = 0, y = 1750, x = 600, radius = 50,
-                                                  material = rayrender::light(color = "white", intensity = 4500)),
-                                              rayrender::text3d(label = "Barents Sea", angle = c(30, 60, 0), z = 500, y = 800, x = -650, 
-                                                  text_height = 150, material = rayrender::light(color = "white", intensity = 100))),  
+render_highquality(print_scene_info = T, parallel = TRUE, lightintensity = 0, samples = 600, filename = "rayshade-hi.png", 
+                   scene_elements = bind_rows(rayrender::sphere(z = -1500, y = 5500, x = 1000, radius = 100,
+                                                                material = rayrender::light(color = "white", intensity = 2500)),
+                                              rayrender::text3d(label = "Brazilian Shelf", angle = c(-30, 210, 0), z = 6000, y = 1600, x = -1200, 
+                                                                text_height = 400, material = rayrender::light(color = "white", intensity = 100))),  
                    clamp_value = 2, aperture = 120, # Bigger aperture, more blur
                    min_variance = 0)
 toc()
