31dd2758e6e73b5f205ed5fd4edeae727da14951
Jack H Laverick from Strath <jacklaverick@ymail.com>
2022-09-09 11:38:35 +0100

    feat: autocreate results folder

--- R scripts/se2e/strathE2E.01 INITIALISE MODEL.R
+++ R scripts/se2e/strathE2E.01 INITIALISE MODEL.R
@@ -3,5 +3,8 @@
 
 library(ggplot2) ; source("./R scripts/@_Region file.R") # ggplot2 is needed to source the Region file
 
-R.utils::copyDirectory("../Celtic Sea/Data/Celtic_Sea_ERSEM_4/2003-2013/",   # Copy example model 
+R.utils::copyDirectory("../Celtic Sea/Data/Celtic_Sea_ERSEM_4/2003-2013/",              # Copy example model 
                        stringr::str_glue("./StrathE2E/{implementation}/2010-2019/"))    # Into new implementation
+
+dir.create("./StrathE2E/Results")                                                       # Create results folder for model runs
+  
